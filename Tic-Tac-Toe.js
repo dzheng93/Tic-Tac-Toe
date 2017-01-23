b206740c6d38d007fe556a3e003b12e6806d7e19
Dunstan Zheng <dunstanzheng@gmail.com>
2017-01-23 15:21:47 -0800

    Setup for players to choose moves with board status per round

--- Tic-Tac-Toe.js
+++ Tic-Tac-Toe.js
@@ -3,16 +3,33 @@ var startGame = function() {
   // instantiate board with all null's, meaning none are placed yet
   // o === o
   // x === x
+  var finished = false;
+  var currentPlayer = player1;
   var board = [];
   for (var i = 0; i < 3; i++) {
     board.push(Array(3).fill(null));
   }
 
-  // show whether or not it player 1 or player 2's turn.
-  var player1 = prompt('Enter player 1 name', 'player1');
-  var player2 = prompt('Enter player 2 name', 'player2');
+  // assign players & show whether or not it player 1 or player 2's turn.
+  var player1;
+  var player2;
 
-  // add an eventhandler on each spot inside the board, to detect what spot was filled up.
+  while (finished === false) {
+    var currentRow = prompt('Pick the row you would like to place your move, first row starts at 0', '0');
+    var currentColumn = prompt('Pick the column you would like to place your move, first column starts at 0', '0');
+    if (currentPlayer = player1) {
+      board[parseInt(currentRow)][parseInt(currentColumn)] = 'x';
+      currentPlayer = player2;
+      console.log('Board status - ', board);
+      continue;
+    }
+    if (currentPlayer = player2) {
+      board[parseInt(currentRow)][parseInt(currentColumn)] = 'o';
+      currentPlayer = player1;
+      console.log('Board status - ', board);
+      continue;
+    }
 
-// check for row/column/diagonal conflicts for o's & x's to see if there is a winner after each turn.
+  // check for row/column/diagonal conflicts for o's & x's to see if there is a winner after each turn.
+  }
 };
