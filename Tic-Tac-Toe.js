fc7bfff4b69de1693d9b50053f6e113427dde333
Dunstan Zheng <dunstanzheng@gmail.com>
2017-01-23 15:05:00 -0800

    Event handling for individual spots on a board

--- Tic-Tac-Toe.js
+++ Tic-Tac-Toe.js
@@ -1,13 +1,16 @@
-// instantiate board with all null's, meaning none are placed yet
-// 0 === o
-// 1 === x
 
-var board = [];
+var startGame = function() {
+  // instantiate board with all null's, meaning none are placed yet
+  // o === o
+  // x === x
+  var board = [];
+  for (var i = 0; i < 3; i++) {
+    board.push(Array(3).fill(null));
+  }
 
-for (var i = 0; i < 3; i++) {
-  board.push(Array(3).fill(null));
-}
-
-console.log(board);
+  // show whether or not it player 1 or player 2's turn.
+  //
+  // add an eventhandler on each spot inside the board, to detect what spot was filled up.
 
 // check for row/column/diagonal conflicts for o's & x's to see if there is a winner after each turn.
+};
