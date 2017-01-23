9d9ae7c95519d1443d909479ccacdbcdfce5fe76
Dunstan Zheng <dunstanzheng@gmail.com>
2017-01-23 15:10:50 -0800

    Prompt for player information

--- Tic-Tac-Toe.js
+++ Tic-Tac-Toe.js
@@ -9,7 +9,9 @@ var startGame = function() {
   }
 
   // show whether or not it player 1 or player 2's turn.
-  //
+  var player1 = prompt('Enter player 1 name', 'player1');
+  var player2 = prompt('Enter player 2 name', 'player2');
+
   // add an eventhandler on each spot inside the board, to detect what spot was filled up.
 
 // check for row/column/diagonal conflicts for o's & x's to see if there is a winner after each turn.
